91852f1fddbe6eb5b6ca22fd75fa6c6d6e816b42
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-10-24 17:36:11 +0800

    create Dealer.java and GameView.java

---
+++ src/main/java/Dealer.java
@@ -0,0 +1,15 @@
+/**
+ * @author JJAYCHEN
+ * @date 2019/10/24 5:32 下午
+ */
+public class Dealer {
+    Hand hand;
+
+    public void autoHit(){
+        
+    }
+
+    public void clearHand() {
+        hand = null;
+    }
+}
