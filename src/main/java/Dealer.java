c668293628bbd2f1948592cc008faed16c80090b
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-10-24 17:56:46 +0800

    Update Dealer.java

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -29,10 +29,15 @@ public class Dealer {
         hand = new Hand(cardList);
     }
 
+    /**
+     * The dealer is operated by computer. We need autoHit
+     * to automatically choose whether hit or not.
+     * This should be called by BlackJackGame class.
+     */
     public void autoHit() {
         while (hand.calculateValue() < 17) {
             // TODO: get a card from the Deck through BlackJackGame
-            Card card = BlackJackGame.getCard;
+            Card card = BlackJackGame.getCard();
             hand.addCard(card);
         }
     }
