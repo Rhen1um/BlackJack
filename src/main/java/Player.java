70b2fa3e371485360156aae8bd70035f5121de56
mufanx <35735090+mufanx@users.noreply.github.com>
2019-10-24 17:46:29 +0800

    Player2.0

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -6,19 +6,29 @@
 public class Player {
     private int money;
     private PlayerHand[] playerHands;
+    private int handCount;
 
+    /**
+     * Player's init money is 1000
+     */
     Player() {
+        money = 1000;
+        handCount = 0;
+        playerHands = new PlayerHand[5];
+    }
 
+    public PlayerHand[] getPlayerHands() {
+        return playerHands;
     }
 
     public void addHand() {
-
+        
     }
 
     public void hit() {
 
     }
-    
+
     public void clearHands() {
 
     }
