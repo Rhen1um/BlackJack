b9fc0bc4e5fec9c615b7dc7ec2d38a6d1f576208
mufanx <35735090+mufanx@users.noreply.github.com>
2019-10-24 17:37:58 +0800

    Player init

---
+++ src/main/java/Player.java
@@ -0,0 +1,25 @@
+/**
+ * @author YueChen
+ * @version 1.0
+ * @date 2019/10/24 17:34
+ */
+public class Player {
+    private int money;
+    private PlayerHand[] playerHands;
+
+    Player() {
+
+    }
+
+    public void addHand() {
+
+    }
+
+    public void hit() {
+
+    }
+    
+    public void clearHands() {
+
+    }
+}
